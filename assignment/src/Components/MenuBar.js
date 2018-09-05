2cb354ab4df2a7272a6b79582c26903763692310
chetanpatil1 <chetan.patil@sial.com>
2018-09-05 16:09:53 +0530

    changes required

--- assignment/src/Components/MenuBar.js
+++ assignment/src/Components/MenuBar.js
@@ -4,27 +4,39 @@ export default class ModelContainer extends Component {
     render() {
         return (
             <div className='menuStyle'>
-                <ul className='menubar list-group'>
-                    <li className="list-group-item">
-                        <Link to='/people'>{'People'}</Link>
-                    </li>
-                    <li className="list-group-item">
-                        <Link to='/films'>{'Films'}</Link>
-                    </li>
-                    <li className="list-group-item">
-                        <Link to='/starships'>{'Starships'}</Link>
-                    </li>
-                    <li className="list-group-item">
-                        <Link to='/vechicles'>{'Vehicles'}</Link>
-                    </li>
-                    <li className="list-group-item">
-                        <Link to='/species'>{'Species'}</Link>
-                    </li>
-                    <li className="list-group-item">
-                        <Link to='/planets'>{'Planets'}</Link>
-                    </li>
-                </ul>
-            </div>
+            <ul className='menubar list-group'>
+                <Link to='/people'>
+                <li className="list-group-item">
+                    {'People'}
+                </li>
+                </Link>
+                <Link to='/films'>
+                <li className="list-group-item">
+                    {'Films'}
+                </li>
+                </Link>
+                <Link to='/starships'>
+                <li className="list-group-item">
+                    {'Starships'}
+                </li>
+                </Link>
+                <Link to='/vechicles'>
+                <li className="list-group-item">
+                    {'Vehicles'}
+                </li>
+                </Link>
+                <Link to='/species'>
+                <li className="list-group-item">
+                    {'Species'}
+                </li>
+                </Link>
+                <Link to='/planets'>
+                <li className="list-group-item">
+                    {'Planets'}
+                </li>
+                </Link>
+            </ul>
+        </div>
         )
     }
 }
